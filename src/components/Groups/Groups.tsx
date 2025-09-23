65a9c243c8553a347730cbab84e4b99b8276ffee
NewPlace <newplace36214@gmail.com>
2025-09-23 15:41:11 +0700

    Рабочая версия до 4 лабы
    - Нужно удалить init у файла init.evn.local

--- src/components/Groups/Groups.tsx
+++ src/components/Groups/Groups.tsx
@@ -1,34 +1,10 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import type GroupInterface from '@/types/GroupInterface';
+import useGroups from '@/hooks/useGroups';
 import styles from './Groups.module.scss';
 
 const Groups = (): React.ReactElement => {
-  const [groups, setGroups] = useState<GroupInterface[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchGroups = async () => {
-      try {
-        const res = await fetch('/api/groups');
-        if (!res.ok) throw new Error(`Ошибка HTTP: ${res.status}`);
-        const data: GroupInterface[] = await res.json();
-        setGroups(data);
-      } catch (err: unknown) {
-        if (err instanceof Error) setError(err.message);
-        else setError('Неизвестная ошибка при загрузке групп');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchGroups();
-  }, []);
-
-  if (loading) return <div>Загрузка...</div>;
-  if (error) return <div style={{ color: 'red' }}>{error}</div>;
+  const { groups } = useGroups();
 
   return (
     <div className={styles.Groups}>
---
+++ src/hooks/useStudents.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+import { getStudentsApi } from '@/api/studentsApi';
+import type StudentInterface from '@/types/StudentInterface';
+
+interface StudentsHookInterface {
+  students: StudentInterface[];
+}
+
+const useStudents = (): StudentsHookInterface => {
+  const { data } = useQuery({
+    queryKey: ['students'],
+    queryFn: () => getStudentsApi(),
+    enabled: false,
+  });
+
+  return {
+    students: data ?? [],
+  };
+};
+
+export default useStudents;
